af285c4890c301e6be603ad0cb7ccd7043cd9455
1cosmic <notifylore@gmail.com>
2022-05-28 19:46:16 +0000

    Aplha 0.1.

---
+++ nvim/lua/settings.lua
@@ -0,0 +1,240 @@
+local cmd = vim.cmd             -- execute Vim commands
+local exec = vim.api.nvim_exec  -- execute Vimscript
+local g = vim.g                 -- global variables
+local opt = vim.opt             -- global/buffer/windows-scoped options
+-- Компактный вид у тагбара и Отк. сортировка по имени у тагбара
+g.tagbar_compact = 1
+g.tagbar_sort = 0
+
+-- Конфиг ale + eslint
+g.ale_sign_error = '❌'
+g.ale_sign_warning = '⚠️'
+g.ale_fix_on_save = 1
+
+
+-- Главные
+-----------------------------------------------------------
+opt.colorcolumn = '80'              -- Разделитель на 80 символов
+opt.cursorline = true               -- Подсветка строки с курсором
+opt.spelllang= { 'en_us', 'ru' }    -- Словари рус eng
+opt.number = true                   -- Включаем нумерацию строк
+opt.relativenumber = true           -- Вкл. относительную нумерацию строк
+opt.so=999                          -- Курсор всегда в центре экрана
+opt.undofile = true                 -- Возможность отката назад
+opt.splitright = true               -- vertical split вправо
+opt.splitbelow = true               -- horizontal split вниз
+
+cmd([[
+filetype indent plugin on
+syntax enable
+]])
+
+-- Цветовая схема
+-----------------------------------------------------------
+opt.termguicolors = true      --  24-bit RGB colors
+
+
+-- Табы и отступы
+-----------------------------------------------------------
+opt.expandtab = true      -- use spaces instead of tabs
+opt.shiftwidth = 4        -- shift 4 spaces when tab
+opt.tabstop = 4           -- 1 tab == 4 spaces
+opt.smartindent = true    -- autoindent new lines
+-- don't auto commenting new lines
+cmd [[au BufEnter * set fo-=c fo-=r fo-=o]]
+
+
+-- Полезные фишки
+-----------------------------------------------------------
+-- Запоминает где nvim последний раз редактировал файл
+cmd [[
+autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+]]
+-- Подсвечивает на доли секунды скопированную часть текста
+exec([[
+augroup YankHighlight
+autocmd!
+autocmd TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=700}
+augroup end
+]], false)
+
+
+-----------------------------------------------------------
+-- Установки для плагинов
+-----------------------------------------------------------
+-- LSP settings
+local lsp_installer = require("nvim-lsp-installer")
+lsp_installer.on_server_ready(function(server)
+    local opts = {}
+    if server.name == "sumneko_lua" then
+        -- only apply these settings for the "sumneko_lua" server
+        opts.settings = {
+            Lua = {
+                diagnostics = {
+                    -- Get the language server to recognize the 'vim', 'use' global
+                    globals = {'vim', 'use'},
+                },
+                workspace = {
+                    -- Make the server aware of Neovim runtime files
+                    library = vim.api.nvim_get_runtime_file("", true),
+                },
+                -- Do not send telemetry data containing a randomized but unique identifier
+                telemetry = {
+                    enable = false,
+                },
+            },
+        }
+    end
+    server:setup(opts)
+end)
+
+
+-- nvim-cmp supports additional completion capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+vim.o.completeopt = 'menuone,noselect'
+-- luasnip setup
+local luasnip = require 'luasnip'
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'path' },
+        { name = 'buffer', settings = {
+            get_bufnrs = function()
+                return vim.api.nvim_list_bufs()
+            end
+        },
+    },
+},
+}
+
+
+-- NVIM Tree settings.
+-- each of these are documented in `:help nvim-tree.OPTION_NAME`
+-- nested options are documented by accessing them with `.` (eg: `:help nvim-tree.view.mappings.list`).
+require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
+  auto_reload_on_write = true,
+  disable_netrw = false,
+  hijack_cursor = false,
+  hijack_netrw = true,
+  hijack_unnamed_buffer_when_opening = false,
+  ignore_buffer_on_setup = false,
+  open_on_setup = false,
+  open_on_setup_file = false,
+  open_on_tab = false,
+  sort_by = "name",
+  update_cwd = false,
+  reload_on_bufenter = false,
+  view = {
+    width = 30,
+    height = 30,
+    hide_root_folder = false,
+    side = "left",
+    preserve_window_proportions = false,
+    number = false,
+    relativenumber = false,
+    signcolumn = "yes",
+    mappings = {
+      custom_only = false,
+      list = {
+        -- user mappings go here
+      },
+    },
+  },
+  renderer = {
+    indent_markers = {
+      enable = false,
+      icons = {
+        corner = "└ ",
+        edge = "│ ",
+        none = "  ",
+      },
+    },
+    icons = {
+      webdev_colors = true,
+      git_placement = "before",
+    },
+  },
+  hijack_directories = {
+    enable = true,
+    auto_open = true,
+  },
+  update_focused_file = {
+    enable = false,
+    update_cwd = false,
+    ignore_list = {},
+  },
+  ignore_ft_on_setup = {},
+  system_open = {
+    cmd = "",
+    args = {},
+  },
+  diagnostics = {
+    enable = false,
+    show_on_dirs = false,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    },
+  },
+  filters = {
+    dotfiles = false,
+    custom = {},
+    exclude = {},
+  },
+  git = {
+    enable = true,
+    ignore = true,
+    timeout = 400,
+  },
+  actions = {
+    use_system_clipboard = true,
+    change_dir = {
+      enable = true,
+      global = false,
+      restrict_above_cwd = false,
+    },
+    open_file = {
+      quit_on_open = false,
+      resize_window = true,
+      window_picker = {
+        enable = true,
+        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+        exclude = {
+          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+          buftype = { "nofile", "terminal", "help" },
+        },
+      },
+    },
+  },
+  trash = {
+    cmd = "trash",
+    require_confirm = true,
+  },
+  live_filter = {
+    prefix = "[FILTER]: ",
+    always_show_folders = true,
+  },
+  log = {
+    enable = false,
+    truncate = false,
+    types = {
+      all = false,
+      config = false,
+      copy_paste = false,
+      diagnostics = false,
+      git = false,
+      profile = false,
+    },
+  },
+} -- END_DEFAULT_OPTS
